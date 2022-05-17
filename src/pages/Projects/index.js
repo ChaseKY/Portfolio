1da1952f6b682aff7a30d5338f6827639d9d9aa3
Chase Young <chasekyoung@gmail.com>
2022-05-17 14:16:07 -0400

    projects with related pictures added to projects page

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,7 +1,52 @@
 import React from 'react';
+import { Card, Button, Container } from 'react-bootstrap';
 
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div>
+      <Container className="cardContainer">
+        <Card className="card" style={{ width: '18rem', backgroundColor: 'lightblue' }}>
+          <Card.Img className="image" variant="top" src="/Homepage2.png" />
+          <Card.Body>
+            <Card.Title>Film Finder</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of the card's
+              content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card className="card" style={{ width: '18rem', backgroundColor: 'lightblue' }}>
+          <Card.Img className="image" variant="top" src="/logo-text.png" />
+          <Card.Body>
+            <Card.Title>Hamper</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of the card's
+              content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card className="card" style={{ width: '18rem', backgroundColor: 'lightblue' }}>
+          <Card.Img
+            className="image"
+            variant="top"
+            src="https://cdn-icons-png.flaticon.com/512/747/747775.png"
+          />
+          <Card.Body>
+            <Card.Title>Daily Planner</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of the card's
+              content.
+            </Card.Text>
+            <Button href="https://chaseky.github.io/dailyPlanner/" variant="primary">
+              Go to app
+            </Button>
+          </Card.Body>
+        </Card>
+      </Container>
+    </div>
+  );
 }
 
 export default Projects;
